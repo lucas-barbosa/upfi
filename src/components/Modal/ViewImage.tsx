7fa4b94c01edb5bd66af50de66b0f62083a80ef8
Lucas Barbosa <barbosa.lucash@gmail.com>
2022-03-19 11:34:44 -0300

    feat: add modal view image component

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,26 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+
+      <ModalContent maxW="900px" maxH="600px" w="auto" h="auto">
+        <ModalBody p="0">
+          <Image src={imgUrl} maxW="900px" maxH="600px" />
+        </ModalBody>
+
+        <ModalFooter
+          bg="pGray.800"
+          justifyContent="flex-start"
+          p="0"
+          borderRadius="0 0 6px 6px"
+        >
+          <Link href={imgUrl} py="8px" px="10px" color="pGray.50" isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
